c727ae92e8ee68acdf175c8150039c53b3d4228d
Carlos Chacin <chacin.carlos9798@gmail.com>
2016-10-15 06:28:39 -0400

    Add files via upload
    Updated HTML and added more controls.

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 var socket = io();//auto-discovery
+$(document).ready(function() {
 //////////////////////
 ///Event Listeners
 /////////////////////
@@ -8,28 +9,69 @@ var socket = io();//auto-discovery
 		document.getElementById("users-connected-update").innerHTML = users;
 	});
 //////////////////////
-///Remote Setup
+///Navigation Setup
 /////////////////////
-//Set CLI Command
-var device = "Comcast";
-var command = "irsend SEND_ONCE "+device+" ";
-//Sets Remote Buttons After HTML Loads
-$(document).ready(function() {
+	currentNav = "#nav-item1";//initial tab
+	currentLayout = "#numpad-layout";//initial content
+	$(currentLayout).show();//unhide initial content
+
+	$("#nav-item1").on("click", function(){changeLayout("#nav-item1","#numpad-layout");});
+	$("#nav-item2").on("click", function(){changeLayout("#nav-item2","#controls-layout");});
+//////////////////////
+///Remote Setup 
+/////////////////////
+	//Set CLI Command
+	var device = "Comcast";
+	var command = "irsend SEND_ONCE "+device+" ";
+
 	//Numpad Bindings
 	for(var i = 0; i<10; i++){
 		setNumPad(i);
 	}
-});
+	//Misc
+	$( "#button-power" ).on( "click", function(){remoteCommand("KEY_POWER");});
+	$( "#button-info" ).on( "click", function(){remoteCommand("KEY_INFO");});
+	$( "#button-last" ).on( "click", function(){remoteCommand("KEY_Last");});
+	$( "#button-enter" ).on( "click", function(){remoteCommand("KEY_Enter");});
+	//Channel Control
+	$( "#button-channel-up" ).on( "click", function(){remoteCommand("KEY_CHANNELUP");});
+	$( "#button-channel-down" ).on( "click", function(){remoteCommand("KEY_CHANNELDOWN");});
+	//volume control
+	$( "#button-mute" ).on( "click", function(){remoteCommand("KEY_MUTE");});
+	$( "#button-volume-up" ).on( "click", function(){remoteCommand("KEY_VOLUMEUP");});
+	$( "#button-volume-down" ).on( "click", function(){remoteCommand("KEY_VOLUMEDOWN");});
+	//Movement Keys
+	$( "#button-ok" ).on( "click", function(){remoteCommand("KEY_OK");});
+	$( "#button-up" ).on( "click", function(){remoteCommand("KEY_UP");});
+	$( "#button-down" ).on( "click", function(){remoteCommand("KEY_DOWN");});
+	$( "#button-left" ).on( "click", function(){remoteCommand("KEY_LEFT");});
+	$( "#button-right" ).on( "click", function(){remoteCommand("KEY_RIGHT");});
+	//Missing KEY_GUIDE....
+	//Missing KEY_EXIT.....
+
 //////////////////////
 ///Functions
 /////////////////////
-//Sets NumPad Keys
-function setNumPad(num){
-	$( "#button-"+num ).on( "click", function(){remoteCommand("KEY_"+num);});
-}
-//sends ir signal command
-function remoteCommand(buttonPressed){
-    socket.emit('remoteCommand', command+buttonPressed);
-}
+	//Changes Controls (updates nav and current buttons shown)
+	function changeLayout(nav, layout){
+		if(currentNav != nav){
+			$(currentNav).css( "background-color", "transparent" );
+			currentNav = nav;
+			$(currentNav).css( "background-color", "rgba(255, 255, 255, 0.50)" );
+
+			$(currentLayout).hide();
+			currentLayout = layout;
+			$(currentLayout).show();
+		}
+	}
+	//Sets NumPad Keys
+	function setNumPad(num){
+		$( "#button-"+num ).on( "click", function(){remoteCommand("KEY_"+num);});
+	}
+	//sends ir signal command
+	function remoteCommand(buttonPressed){
+	    socket.emit('remoteCommand', command+buttonPressed);
+	}
+});
 
 
